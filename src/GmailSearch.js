48548b2b0dbabe8837612dfa207ca4cfab1232c0
Nagendra <nagendra.mohan.5@gmail.com>
2026-01-11 16:58:37 +0530

    almost done

--- src/GmailSearch.js
+++ src/GmailSearch.js
@@ -1,33 +1,73 @@
-import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TextInput, FlatList, Pressable } from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react'
 
 const GmailSearch = () => {
     const [data, setData] = useState([])
     const [filteredTxt, setFilteredTxt] = useState([])
     const [searchTxt, setSearchTxt] = useState("")
-    const [showPopup, setShoPopup] = useState(true)
+
 
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('https://jsonplaceholder.typicode.com/posts')
             const resData = await res.json()
-            setData(resData)
+            setData(resData.map((item) => ({
+                ...item,
+                isSelected: false
+            })))
         }
         fetchData()
     }, [])
 
-    const renderPopupData = ({ item, index }) => {
+    const highlightText = (text, search) => {  //practice again
+        if (!search) return <Text>{text}</Text>
+
+        let lowerCaseText = text.toLowerCase()
+        let lowerCaseSearch = search.toLowerCase()
+
+        let startingIndex = lowerCaseText?.indexOf(lowerCaseSearch)
+
+        if (startingIndex === -1) return <Text>{text}</Text>
+
+        let endIndex = startingIndex + search?.length
+
         return (
-            <View style={{
-                backgroundColor: index % 2 === 0 ? '#A8DF8E' : '#F0FFDF',
-                paddingVertical:8,
-                paddingLeft:2,
-                borderRadius:2
-            }}>
-                <Text>{item?.title}</Text>
-            </View>
+            <Text>
+                <Text>{text.substring(0, startingIndex)}</Text>
+                <Text style={{ backgroundColor: '#FFD54F', fontWeight: '600' }}>
+                    {text.substring(startingIndex, endIndex)}
+                </Text>
+                <Text>{text.substring(endIndex, text.lengthd)}</Text>
+            </Text>
         )
+    };
+
+    const onSelect = (item,index) => {
+        setFilteredTxt((prev) => prev?.map((item,i) => i === index ? {...item, isSelected : !item?.isSelected} : item))
     }
+
+    const renderPopupData = useCallback(
+        ({ item, index }) => (
+            <Pressable style={{ backgroundColor: item?.isSelected ? '#FFAAB8' :  index % 2 === 0 ? '#A8DF8E' : '#F0FFDF', paddingVertical: 8 }}
+                onPress={() => onSelect(item,index)}
+            >
+                {highlightText(item.title, searchTxt)}
+            </Pressable>
+        ),
+        [searchTxt]
+    );
+
+    const onChangeSearchText = (text) => {
+        setSearchTxt(text);
+
+        const filtered = data.filter((item) =>
+            item.title.toLowerCase().includes(text.toLowerCase())
+        );
+
+        setFilteredTxt(() => data.filter((item) =>
+            item.title.toLowerCase().includes(text.toLowerCase())
+        ));
+    };
     return (
         <View style={styles.container}>
             <View>
@@ -35,11 +75,11 @@ const GmailSearch = () => {
                     value={searchTxt}
                     style={styles.searchBox}
                     placeholder='Search....'
-                    onChangeText={(txt) => setSearchTxt(txt)}
+                    onChangeText={(txt) => onChangeSearchText(txt)}
                 />
             </View>
-            <View style={{ width: 300, height:660}}>
-                {showPopup ?
+            <View style={{ width: 300, height: 660 }}>
+                {searchTxt?.length ?
                     <FlatList
                         data={filteredTxt}
                         renderItem={renderPopupData}
